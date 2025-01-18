f80254a996412dd9d567fa989ab3795ec2d7050a
meghwardayanand <meghwardayanand@gmail.com>
2025-01-18 00:54:28 -0500

    ENHANCEMENT: Add isPalindrome in Reverse String

--- applications/stack/reverseString/reverseString.py
+++ applications/stack/reverseString/reverseString.py
@@ -6,9 +6,17 @@ class String:
         self.string = string
         self.length = len(string)
         self.stack = Stack(self.length, dtypes=[str])
+        self.specials = "@_!#$%^&*()<>?/\|}{~:, "
 
-    def reverse(self):
+    def reverse(self, ignore_specials=False):
         for char in self.string:
+            if ignore_specials and char in self.specials:
+                continue
+
             self.stack.push(char)
 
-        return "".join([self.stack.pop() for _ in range(self.length)])
+        return "".join([self.stack.pop() for _ in range(self.length) if not self.stack.isEmpty()])
+
+    def isPalindrome(self):
+        actual_string = "".join([char.lower() for char in self.string if char not in self.specials])
+        return actual_string == self.reverse(ignore_specials=True).lower()
