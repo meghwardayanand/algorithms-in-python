fe06306dda8b0dc038c3db8dc446a8a8f9c9c575
Dayanand <meghwardayanand@gmail.com>
2025-01-20 03:35:37 -0500

    ENHANCEMENT: Added Directed & Weighted Features For Graphs (#26)
    * ENHANCEMENT: Added Directed & Weighted Features For Graphs
    
    * FIX: Tuple Extraction

--- graphs/graph/graph.py
+++ graphs/graph/graph.py
@@ -1,15 +1,17 @@
 
 class Graph:
-    def __init__(self, representation='list'):
+    def __init__(self, representation='list', is_directed=False, is_weighted=False):
         if representation not in ['list', 'matrix']:
             raise ValueError("Representation must be either 'list' or 'matrix'")
 
         self.representation = representation
+        self.is_directed = is_directed
+        self.is_weighted = is_weighted
         self.graph = {}     # For adjacency list representation
         self.matrix = []    # For adjacency matrix representation
         self.vertices = []  # List to hold vertices for matrix index mapping
 
-    def add_vertex(self, vertex):
+    def addVertex(self, vertex):
         if self.representation == 'list':
             if vertex not in self.graph:
                 self.graph[vertex] = []
@@ -24,36 +26,45 @@ class Graph:
 
                 self.matrix.append([0] * size)
 
-    def add_edge(self, vertex1, vertex2):
+    def addEdge(self, vertex1, vertex2, weight=None):
         if self.representation == 'list':
             if vertex1 not in self.graph:
-                self.add_vertex(vertex1)
+                self.addVertex(vertex1)
 
             if vertex2 not in self.graph:
-                self.add_vertex(vertex2)
+                self.addVertex(vertex2)
 
-            self.graph[vertex1].append(vertex2)
-            self.graph[vertex2].append(vertex1)
+            one_way_edge = (vertex2, weight) if self.is_weighted else vertex2
+            two_way_edge = (vertex1, weight) if self.is_weighted else vertex1
+            self.graph[vertex1].append(one_way_edge)
+            if not self.is_directed:
+                self.graph[vertex2].append(two_way_edge)
 
         elif self.representation == 'matrix':
             if vertex1 not in self.vertices:
-                self.add_vertex(vertex1)
+                self.addVertex(vertex1)
 
             if vertex2 not in self.vertices:
-                self.add_vertex(vertex2)
+                self.addVertex(vertex2)
 
             index1 = self.vertices.index(vertex1)
             index2 = self.vertices.index(vertex2)
-            self.matrix[index1][index2] = 1
-            self.matrix[index2][index1] = 1
+            self.matrix[index1][index2] = weight if self.is_weighted else 1
+            if not self.is_directed:
+                self.matrix[index2][index1] = weight if self.is_weighted else 1
 
-    def remove_vertex(self, vertex):
+    def removeVertex(self, vertex):
         if self.representation == 'list':
-            if vertex in self.graph:
-                for neighbor in self.graph[vertex]:
-                    self.graph[neighbor].remove(vertex)
+            if vertex not in self.graph:
+                return
 
-                del self.graph[vertex]
+            if not self.is_directed:
+                for neighborhood in self.graph[vertex]:
+                    neighbor, weight = neighborhood if self.is_weighted else (neighborhood, None)
+                    connection = (vertex, weight) if self.is_weighted else vertex
+                    self.graph[neighbor].remove(connection)
+
+            del self.graph[vertex]
 
         elif self.representation == 'matrix':
             if vertex in self.vertices:
@@ -64,20 +75,40 @@ class Graph:
                 for row in self.matrix:
                     row.pop(index)
 
-    def remove_edge(self, vertex1, vertex2):
-        if self.representation == 'list':
-            if vertex1 in self.graph and vertex2 in self.graph[vertex1]:
-                self.graph[vertex1].remove(vertex2)
+    def _getEdge(self, from_vertex, to_vertex):
+        for neighborhood in self.graph[from_vertex]:
+            (neighbor, weight) = neighborhood if self.is_weighted else (neighborhood, None)
+            if neighbor != to_vertex:
+                continue
 
-            if vertex2 in self.graph and vertex1 in self.graph[vertex2]:
-                self.graph[vertex2].remove(vertex1)
+            return (neighbor, weight) if self.is_weighted else neighbor
+
+    def removeEdge(self, vertex1, vertex2):
+        if self.representation == 'list':
+            if vertex1 in self.graph:
+                edge = self._getEdge(vertex1, vertex2)
+                if not edge:
+                    return
+
+                self.graph[vertex1].remove(edge)
+
+            if self.is_directed:
+                return
+
+            if vertex2 in self.graph:
+                edge = self._getEdge(vertex2, vertex1)
+                if not edge:
+                    return
+
+                self.graph[vertex2].remove(edge)
 
         elif self.representation == 'matrix':
             if vertex1 in self.vertices and vertex2 in self.vertices:
                 index1 = self.vertices.index(vertex1)
                 index2 = self.vertices.index(vertex2)
                 self.matrix[index1][index2] = 0
-                self.matrix[index2][index1] = 0
+                if not self.is_directed:
+                    self.matrix[index2][index1] = 0
 
     def display(self):
         print(self.__repr__())
--- graphs/graph/tests.py
+++ graphs/graph/tests.py
@@ -19,20 +19,20 @@ class TestGraph(unittest.TestCase):
 
     def test_add_vertex_list(self):
         graph = Graph(representation='list')
-        graph.add_vertex("A")
+        graph.addVertex("A")
         self.assertIn("A", graph.graph)
         self.assertEqual(graph.graph["A"], [])
 
     def test_add_vertex_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_vertex("A")
+        graph.addVertex("A")
         self.assertIn("A", graph.vertices)
         self.assertEqual(len(graph.matrix), 1)
         self.assertEqual(len(graph.matrix[0]), 1)
 
     def test_add_edge_list(self):
         graph = Graph(representation='list')
-        graph.add_edge("A", "B")
+        graph.addEdge("A", "B")
         self.assertIn("A", graph.graph)
         self.assertIn("B", graph.graph)
         self.assertIn("B", graph.graph["A"])
@@ -40,7 +40,7 @@ class TestGraph(unittest.TestCase):
 
     def test_add_edge_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_edge("A", "B")
+        graph.addEdge("A", "B")
         self.assertIn("A", graph.vertices)
         self.assertIn("B", graph.vertices)
         index1 = graph.vertices.index("A")
@@ -48,37 +48,60 @@ class TestGraph(unittest.TestCase):
         self.assertEqual(graph.matrix[index1][index2], 1)
         self.assertEqual(graph.matrix[index2][index1], 1)
 
+    def test_add_edge_directed(self):
+        graph = Graph(representation='list', is_directed=True)
+        graph.addEdge("A", "B")
+        self.assertIn("B", graph.graph["A"])
+        self.assertNotIn("A", graph.graph["B"])
+
+    def test_add_edge_weighted(self):
+        graph = Graph(representation='list', is_weighted=True)
+        graph.addEdge("A", "B", weight=5)
+        graph.addEdge("A", "C", weight=3)
+        self.assertIn(("B", 5), graph.graph["A"])
+        self.assertIn(("C", 3), graph.graph["A"])
+
+    def test_add_edge_weighted_matrix(self):
+        graph = Graph(representation='matrix', is_weighted=True)
+        graph.addEdge("A", "B", weight=5)
+        graph.addEdge("A", "C", weight=3)
+        index1 = graph.vertices.index("A")
+        index2 = graph.vertices.index("B")
+        index3 = graph.vertices.index("C")
+        self.assertEqual(graph.matrix[index1][index2], 5)
+        self.assertEqual(graph.matrix[index1][index3], 3)
+
     def test_remove_vertex_list(self):
         graph = Graph(representation='list')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
-        graph.remove_vertex("B")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.removeVertex("B")
         self.assertNotIn("B", graph.graph)
         self.assertNotIn("B", graph.graph["A"])
         self.assertNotIn("B", graph.graph["C"])
 
     def test_remove_vertex_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
-        graph.remove_vertex("B")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.removeVertex("B")
         self.assertNotIn("B", graph.vertices)
         self.assertEqual(len(graph.matrix), 2)
         self.assertEqual(len(graph.matrix[0]), 2)
 
     def test_remove_edge_list(self):
         graph = Graph(representation='list')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
-        graph.remove_edge("A", "B")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.removeEdge("A", "B")
         self.assertNotIn("B", graph.graph["A"])
         self.assertNotIn("A", graph.graph["B"])
 
     def test_remove_edge_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
-        graph.remove_edge("A", "B")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.removeEdge("A", "B")
         index1 = graph.vertices.index("A")
         index2 = graph.vertices.index("B")
         self.assertEqual(graph.matrix[index1][index2], 0)
@@ -86,16 +109,16 @@ class TestGraph(unittest.TestCase):
 
     def test_repr_list(self):
         graph = Graph(representation='list')
-        graph.add_edge("A", "B")
-        graph.add_edge("A", "C")
+        graph.addEdge("A", "B")
+        graph.addEdge("A", "C")
         repr_output = repr(graph)
         self.assertIn("A: ['B', 'C']", repr_output)
         self.assertIn("B: ['A']", repr_output)
 
     def test_repr_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_edge("A", "B")
-        graph.add_edge("A", "C")
+        graph.addEdge("A", "B")
+        graph.addEdge("A", "C")
         repr_output = repr(graph)
         self.assertIn("Adjacency Matrix:", repr_output)
         self.assertIn("A 0 1 1", repr_output)
@@ -103,8 +126,8 @@ class TestGraph(unittest.TestCase):
 
     def test_edge_and_vertex_consistency_list(self):
         graph = Graph(representation='list')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
         self.assertIn("A", graph.graph)
         self.assertIn("B", graph.graph)
         self.assertIn("C", graph.graph)
@@ -113,8 +136,8 @@ class TestGraph(unittest.TestCase):
 
     def test_edge_and_vertex_consistency_matrix(self):
         graph = Graph(representation='matrix')
-        graph.add_edge("A", "B")
-        graph.add_edge("B", "C")
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
         self.assertIn("A", graph.vertices)
         self.assertIn("B", graph.vertices)
         self.assertIn("C", graph.vertices)
@@ -123,6 +146,32 @@ class TestGraph(unittest.TestCase):
         index3 = graph.vertices.index("C")
         self.assertEqual(graph.matrix[index1][index2], 1)
         self.assertEqual(graph.matrix[index2][index3], 1)
+
+    def test_remove_edge_directed(self):
+        graph = Graph(representation='list', is_directed=True)
+        graph.addEdge("A", "B")
+        graph.addEdge("B", "C")
+        graph.removeEdge("A", "B")
+        self.assertNotIn("B", graph.graph["A"])
+        self.assertIn("C", graph.graph["B"])
+
+    def test_remove_edge_weighted(self):
+        graph = Graph(representation='list', is_weighted=True)
+        graph.addEdge("A", "B", weight=10)
+        graph.addEdge("A", "C", weight=5)
+        graph.removeEdge("A", "B")
+        self.assertNotIn(("B", 10), graph.graph["A"])
+        self.assertIn(("C", 5), graph.graph["A"])
+
+    def test_remove_edge_weighted_matrix(self):
+        graph = Graph(representation='matrix', is_weighted=True)
+        graph.addEdge("A", "B", weight=10)
+        graph.addEdge("A", "C", weight=5)
+        graph.removeEdge("A", "B")
+        index1 = graph.vertices.index("A")
+        index2 = graph.vertices.index("B")
+        self.assertEqual(graph.matrix[index1][index2], 0)
+        self.assertEqual(graph.matrix[index2][index1], 0)
 
 
 if __name__ == '__main__':
